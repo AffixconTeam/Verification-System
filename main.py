c73dd21dd67ff2d89ba49ed3d4eeee6ab17362d6
TuanMinhajAffixcon <tuan.seedin@affixcon.com.au>
2024-09-23 18:49:37 +0530

    files added

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import textdistance
 import pandas as pd
-# from template import conn
+from template import conn
 from utils import apply_name_matching, address_parsing  # Import necessary functions
 import snowflake.connector
 from fastapi import FastAPI, HTTPException
@@ -10,14 +10,7 @@ import uvicorn
 
 app = FastAPI()
 
-conn = snowflake.connector.connect(
-    user='TuanSeedin',
-    password='J!2af!G1j',
-    account='hg21418.ap-south-1',
-    warehouse='COMPUTE_WH',
-    database='DATA_VERIFICATION',
-    schema='PUBLIC',
-    role='ACCOUNTADMIN')
+
 
 class UserData(BaseModel):
     first_name: str
@@ -36,30 +29,30 @@ def verify_user(data: UserData):
     try:
         cursor = conn.cursor()
 
-        # query = f"""
-        #     WITH InputData AS (
-        #         SELECT
-        #             '{data['first_name']}' AS first_name_input,
-        #             '{data['middle_name']}' AS middle_name_input,
-        #             '{data['sur_name']}' AS sur_name_input,
-        #             '{data['dob']}' AS dob_input
-        #     )
-        #     SELECT
-        #         First_name, middle_name, sur_name, dob, ad1, suburb, state, postcode, PHONE2_MOBILE, EMAILADDRESS
-        #     FROM
-        #         DATA_VERIFICATION.PUBLIC.AU_RESIDENTIAL AS resident,
-        #         InputData AS input
-        #     WHERE
-        #         (
-        #             (LOWER(input.sur_name_input) IS NOT NULL AND LOWER(input.sur_name_input) != '' AND LOWER(resident.sur_name) LIKE LOWER(input.sur_name_input))
-        #             OR (LOWER(input.middle_name_input) IS NOT NULL AND LOWER(input.middle_name_input) != '' AND LOWER(resident.middle_name) = LOWER(input.middle_name_input))
-        #             OR (LOWER(input.first_name_input) IS NOT NULL AND LOWER(input.first_name_input) != '' AND LOWER(resident.first_name) = LOWER(input.first_name_input))
-        #             AND (input.dob_input IS NOT NULL AND input.dob_input != '' AND resident.DOB = input.dob_input)
-        #         )
-        #     LIMIT 1
-        # """
+        query = f"""
+            WITH InputData AS (
+                SELECT
+                    '{data['first_name']}' AS first_name_input,
+                    '{data['middle_name']}' AS middle_name_input,
+                    '{data['sur_name']}' AS sur_name_input,
+                    '{data['dob']}' AS dob_input
+            )
+            SELECT
+                First_name, middle_name, sur_name, dob, ad1, suburb, state, postcode, PHONE2_MOBILE, EMAILADDRESS
+            FROM
+                DATA_VERIFICATION.PUBLIC.AU_RESIDENTIAL AS resident,
+                InputData AS input
+            WHERE
+                (
+                    (LOWER(input.sur_name_input) IS NOT NULL AND LOWER(input.sur_name_input) != '' AND LOWER(resident.sur_name) LIKE LOWER(input.sur_name_input))
+                    OR (LOWER(input.middle_name_input) IS NOT NULL AND LOWER(input.middle_name_input) != '' AND LOWER(resident.middle_name) = LOWER(input.middle_name_input))
+                    OR (LOWER(input.first_name_input) IS NOT NULL AND LOWER(input.first_name_input) != '' AND LOWER(resident.first_name) = LOWER(input.first_name_input))
+                    AND (input.dob_input IS NOT NULL AND input.dob_input != '' AND resident.DOB = input.dob_input)
+                )
+            LIMIT 1
+        """
 
-        query = "select * from DATA_VERIFICATION.PUBLIC.AU_RESIDENTIAL AS resident limit 1"
+        # query = "select * from DATA_VERIFICATION.PUBLIC.AU_RESIDENTIAL AS resident limit 1"
 
         cursor.execute(query)
         df = cursor.fetch_pandas_all()
@@ -67,23 +60,23 @@ def verify_user(data: UserData):
         if df.empty:
             raise HTTPException(status_code=404, detail="No match found")
         
-        result = df.to_dict(orient="records")
+        # result = df.to_dict(orient="records")
 
-        return {"results": result}
+        # return {"results": result}
 
 
-        # # Perform similarity matching
-        # df['full_name_similarity'] = (textdistance.jaro_winkler(df['FIRST_NAME'][0].lower(), data['first_name'].lower()) * 100)
-        # df['address_line_similarity'] = (textdistance.jaro_winkler(df['AD1'][0].lower(), data['address_line1'].lower()) * 100)
+        # Perform similarity matching
+        df['full_name_similarity'] = (textdistance.jaro_winkler(df['FIRST_NAME'][0].lower(), data['first_name'].lower()) * 100)
+        df['address_line_similarity'] = (textdistance.jaro_winkler(df['AD1'][0].lower(), data['address_line1'].lower()) * 100)
 
-        # # More matching logic...
+        # More matching logic...
 
-        # # Return the verification results
-        # return {
-        #     "name_similarity": df['full_name_similarity'][0],
-        #     "address_similarity": df['address_line_similarity'][0],
-        #     "results": df.to_dict()
-        # }
+        # Return the verification results
+        return {
+            "name_similarity": df['full_name_similarity'][0],
+            "address_similarity": df['address_line_similarity'][0]
+            # "results": df.to_dict()
+        }
     
     except snowflake.connector.errors.ProgrammingError as e:
         raise HTTPException(status_code=500, detail=f"Error executing query: {e}")
--- template/__init__.py
+++ template/__init__.py
@@ -1,26 +1,16 @@
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 import os 
 import snowflake.connector
 
 
 
-# load_dotenv()
-# conn = snowflake.connector.connect(
-#     user=os.getenv('user'),
-#     password=os.getenv('password'),
-#     account=os.getenv('account'),
-#     warehouse=os.getenv('warehouse'),
-#     database=os.getenv('database'),
-#     schema=os.getenv('schema'),
-#     role=os.getenv('role')
-#     )
-
+load_dotenv()
 conn = snowflake.connector.connect(
-    user = "TuanSeedin",
-    password = "J!2af!G1j",
-    account = "tannder-lz30435",
-    warehouse = "COMPUTE_WH",
-    database = "DATA_VERIFICATION",
-    schema = "PUBLIC",
-    role = "ACCOUNTADMIN"
-)
+    user=os.getenv('user'),
+    password=os.getenv('password'),
+    account=os.getenv('account'),
+    warehouse=os.getenv('warehouse'),
+    database=os.getenv('database'),
+    schema=os.getenv('schema'),
+    role=os.getenv('role')
+    )
