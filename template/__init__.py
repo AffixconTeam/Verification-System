dbf6b10b047089868eb903f8fca366af7121fa7e
TuanMinhajAffixcon <tuan.seedin@affixcon.com.au>
2024-09-23 15:58:51 +0530

    files added

--- template/__init__.py
+++ template/__init__.py
@@ -5,12 +5,22 @@ import snowflake.connector
 
 
 load_dotenv()
+# conn = snowflake.connector.connect(
+#     user=os.getenv('user'),
+#     password=os.getenv('password'),
+#     account=os.getenv('account'),
+#     warehouse=os.getenv('warehouse'),
+#     database=os.getenv('database'),
+#     schema=os.getenv('schema'),
+#     role=os.getenv('role')
+#     )
+
 conn = snowflake.connector.connect(
-    user=os.getenv('user'),
-    password=os.getenv('password'),
-    account=os.getenv('account'),
-    warehouse=os.getenv('warehouse'),
-    database=os.getenv('database'),
-    schema=os.getenv('schema'),
-    role=os.getenv('role')
-    )
+    user = "TuanSeedin",
+    password = "J!2af!G1j",
+    account = "tannder-lz30435",
+    warehouse = "COMPUTE_WH",
+    database = "DATA_VERIFICATION",
+    schema = "PUBLIC",
+    role = "ACCOUNTADMIN"
+)
