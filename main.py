3a8d0ca597cc14654238ba1abdcd23a29514ab46
TuanMinhajAffixcon <tuan.seedin@affixcon.com.au>
2024-09-23 21:38:27 +0530

    files added

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import textdistance
 import pandas as pd
 from template import conn
-from utils import apply_name_matching, address_parsing  # Import necessary functions
+from utils import *
 import snowflake.connector
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import uvicorn
+from fuzzywuzzy import fuzz
 
 
 app = FastAPI()
@@ -72,35 +73,134 @@ def verify_user(data: UserData):
                 name_Str = apply_name_matching(row, name_Str, db_column, input_field, str_index)
             return name_Str
 
-        name_match_str = df.apply(update_name_str, axis=1)
+        name_match_str = df.apply(update_name_str, axis=1)[0]
 
-        # # # Perform similarity matching
-        first_name_similarity = (textdistance.jaro_winkler(df.FIRST_NAME[0].lower(), data.first_name.lower()) * 100)
-        middle_name_similarity = (textdistance.jaro_winkler(df.MIDDLE_NAME[0].lower(), data.first_name.lower()) * 100)
+        df['name_match_str'] = df.apply(update_name_str, axis=1)
+        df['first_name_similarity'] = df['FIRST_NAME'].apply(lambda x: textdistance.jaro_winkler(x.lower(), data.first_name.lower()) * 100).apply(lambda score: int(score) if score > 65 else 0) 
+        df['middle_name_similarity'] = df['MIDDLE_NAME'].apply(lambda x: textdistance.jaro_winkler(x.lower(), data.middle_name.lower())*100).apply(lambda score: int(score) if score > 65 else 0) 
+        df['sur_name_similarity'] = df['SUR_NAME'].apply(lambda x: textdistance.jaro_winkler(x.lower(), data.sur_name.lower())*100).apply(lambda score: score if int(score) > 65 else 0) 
 
-        daddress_line1_similarity = (textdistance.jaro_winkler(df.AD1[0].lower(), data.address_line1.lower()) * 100)
+        if df['name_match_str'][0][0] == 'T':
+            df['first_name_similarity'] = 100
+        if df['name_match_str'][0][1] == 'T':
+            df['middle_name_similarity'] = 100
+        if df['name_match_str'][0][2] == 'T':
+            df['sur_name_similarity'] = 100
 
-        # # More matching logic...
+        full_name_request = (data.first_name.strip() + " " + data.middle_name.strip() + " "+ data.sur_name.strip()).strip().lower()
+        full_name_matched = (df['FIRST_NAME'][0].strip()+ " "+df['MIDDLE_NAME'][0].strip()+ " "+df['SUR_NAME'][0].strip()).lower()
+        name_obj = Name(full_name_request)
+        
+        # Apply the different matching methods from the Name class
+        match_results = {
+            "Exact Match": (df['name_match_str'] == 'EEE').any(),
+            "Hyphenated Match": name_obj.hyphenated(full_name_matched),
+            "Transposed Match": name_obj.transposed(full_name_matched),
+            "Middle Name Mismatch": df['name_match_str'].str.contains('E.*E$', regex=True).any(),
+            "Initial Match": name_obj.initial(full_name_matched),
+            "SurName only Match": df['name_match_str'].str.contains('^[ETMD].*E$', regex=True).any(),
+            "Fuzzy Match": name_obj.fuzzy(full_name_matched),
+            "Nickname Match": name_obj.nickname(full_name_matched),
+            "Missing Part Match": name_obj.missing(full_name_matched),
+            "Different Name": name_obj.different(full_name_matched)
+        }
+        
+        # Filter out any matches that returned False
+        match_results = {k: v for k, v in match_results.items() if v}
+        top_match = next(iter(match_results.items()), ("No Match Found", ""))
 
-        # # Return the verification results
+        df['Name Match Level'] = top_match[0]
+        
+        df['full_name_similarity'] = (textdistance.jaro_winkler(full_name_request,full_name_matched)*100) 
+        df['full_name_similarity'] = df['full_name_similarity'].apply(lambda score: int(score) if score > 65 else 0)
+        if fuzz.token_sort_ratio(full_name_request,full_name_matched)==100 and top_match[0] !='Exact Match':
+            df['full_name_similarity'] = 100
+            df['Match Level'] = 'Transposed Match'
+        
+        df['dob_match'] = df['DOB'].apply(lambda x: Dob(data.dob).exact(x))
+        address_str = "XXXXXX"
+
+        source = {
+            # 'Gnaf_Pid': address_id,
+            'Ad1': df["AD1"][0],
+            'Suburb': df["SUBURB"][0],
+            'State': df["STATE"][0],
+            'Postcode': str(df["POSTCODE"][0])
+        }
+        source_output = address_parsing(df['AD1'][0])
+        source = {**source, **source_output}
+        # st.write(source)
+
+
+        parsed_address = {
+            # 'Gnaf_Pid': address_id,
+            'Ad1': data.address_line1,
+            'Suburb': data.suburb,
+            'State': data.state,
+            'Postcode': str(data.postcode)
+        }
+        parsed_output = address_parsing(data.address_line1)
+        parsed_address = {**parsed_address, **parsed_output}
+        # st.write(parsed_address)
+
+        address_checker = Address(parsed_address=parsed_address,source_address=source)
+        address_str=address_checker.address_line1_match(address_str)
+        df['Address Matching String'] = address_str
+
+        df['address_line_similarity'] = df['AD1'].apply(lambda x: textdistance.jaro_winkler(x.lower(), data.address_line1.lower()) * 100).apply(lambda score: int(score) if score > 65 else 0) 
+        weight1 = 40 if 90<=df['address_line_similarity'][0] <=100 else 30 if 85<=df['address_line_similarity'][0] <90 else 0 
+        
+        df['suburb_similarity'] = df['SUBURB'].apply(lambda x: textdistance.jaro_winkler(x.lower(), data.suburb.lower()) * 100).apply(lambda score: int(score) if score > 65 else 0) 
+        weight2 = 30 if 90<=df['suburb_similarity'][0] <=100 else 25 if 85<=df['suburb_similarity'][0] <90 else 0 
+        
+        df['state_similarity'] = df['STATE'].apply(lambda x: textdistance.jaro_winkler(x.lower(), data.state.lower()) * 100).apply(lambda score: int(score) if score > 65 else 0) 
+        weight3 = 10 if 90<=df['state_similarity'][0] <=100 else  0
+
+        df['postcde_similarity'] = df['POSTCODE'].astype(str).apply(lambda x: 100 if x == data.postcode else 0) 
+        weight4 = 20 if df['postcde_similarity'][0] ==100 else 0 
+        
+        total_weight = weight1+weight2+weight3+weight4
+        if total_weight > 90:
+            match_level = f'Full Match, {total_weight}'
+        elif 80 <= total_weight <= 90:
+            match_level = f'Partial Match, {total_weight}'
+        else:
+            match_level = 'No Match'
+        df['Address Match Level'] = match_level
+
+        matching_levels = get_matching_level(df,data.dob,data.mobile,data.email,df['full_name_similarity'][0],total_weight)
+        df['Overall Matching Level'] = ', '.join(matching_levels)
+        df["Overall Verified Level"] = append_based_on_verification(df,verified_by=True)
+
+        # st.write("source",source)
+        # st.write("parsed_address",parsed_address)
+        # st.write("address_str",address_str)
+        df_transposed = df.T
+        df_transposed.columns = ['Results']
+
+
         # return {
-        #     "name_similarity": df['first_name_similarity'][0],
-        #     "address_similarity": df['address_line1_similarity'][0]
-        #     # "results": df.to_dict()
+        #     'first_db':df.FIRST_NAME[0].lo,
+        #     'first_input':data.first_name.lower(),
+        #     'name_match_str':name_match_str,
+        #     'first_name_similarity':first_name_similarity,
+        #     'middle_name_similarity':middle_name_similarity,
+        #     "daddress_line1_similarity":daddress_line1_similarity
         # }
-
+    
         return {
-            'first_db':df.FIRST_NAME[0].lower(),
-            'first_input':data.first_name.lower(),
-            'name_match_str':name_match_str,
-            'first_name_similarity':first_name_similarity,
-            'middle_name_similarity':middle_name_similarity,
-            "daddress_line1_similarity":daddress_line1_similarity
-
-
+            'FIRST_NAME':df.FIRST_NAME[0],
+            'MIDDLE_NAME':df.MIDDLE_NAME[0],
+            'SUR_NAME':df.SUR_NAME[0],
+            'DOB':df.DOB[0],
+            'AD1':df.AD1[0],
+            "SUBURB":df.SUBURB[0],
+            'STATE':df.STATE[0],
+            'POSTCODE':df.POSTCODE[0],
+            'PHONE2_MOBILE':df.PHONE2_MOBILE[0],
+            'EMAILADDRESS':df.EMAILADDRESS[0]
 
         }
-    
     except snowflake.connector.errors.ProgrammingError as e:
         raise HTTPException(status_code=500, detail=f"Error executing query: {e}")
 
